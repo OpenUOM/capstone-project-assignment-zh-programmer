1a83502cc59ff53c096599c6c191b9686a708fdf
zh-programmer <zhprogrammer01@gmail.com>
2024-12-31 21:35:18 -0800

    New Change in Database

--- backend/database.js
+++ backend/database.js
@@ -23,7 +23,7 @@ const dbinitialize = async () => {
 }
 
 const readTeachers = async () => {
-    const sql = `SELECT * FROM dummy`
+    const sql = `SELECT * FROM teacher`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
